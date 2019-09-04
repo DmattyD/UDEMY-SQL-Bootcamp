07fb42a1204a1457e962bef0286326cdaf998370
DmattyD <46036547+DmattyD@users.noreply.github.com>
2019-09-03 20:00:29 -0600

    Update Logical Operators - Section 11.sql
    Notes completed

--- Logical Operators - Section 11.sql
+++ Logical Operators - Section 11.sql
@@ -44,3 +44,46 @@ Select * from books WHERE author_lname='Eggers' && released_year > 2010 AND titl
 SELECT * FROM books WHERE author_lname='EGGERS' OR released_year > 2010;
 -- will select all books by Eggers, OR every book published after 2010
 -- can chain multiple OR statements.
+
+
+-- BETWEEN
+-- a combination of WHERE and Less Than or Greater than
+
+-- NOT BETWEEN
+-- everything outside of the selected bounds
+
+-- CAST() - can change string values to DATE, tTIME, or DATETIME
+
+SELECT CAST('2017-05-02' AS DATETIME);
+-- will result in 2017-05-02 00:00:00
+
+-- IN / NOT IN
+SELECT title, author_lname FROM books WHERE author_lname IN ('Carver', 'Lahiri', 'Smith');
+-- will return all books written by the above 3 authors
+-- faster than multiple OR statements
+
+-- NOT IN - excludes values from the query
+
+-- % or MODULO
+-- tests for remainder
+SELECT title, released_year FROM books WHERE released_year >= 2000 AND released_year & 2 != 0;
+-- returns all books released in odd years after 2000
+
+-- CASE Statments
+SELECT title, rleased_year, 
+   CASE 
+    WHEN released_year >= 2000 
+      THEN 'Modern Lit'
+      ELSE '20th Century Lit' 
+    END AS GENRE 
+  FROM books;
+  -- will add a genre category to the table based on published year
+  
+  SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity <= 50 THEN '*'
+        WHEN stock_quantity <= 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books; 
+--- will add Stock category with appropriate number of *'s depending on stock_quantity.
