242b8cb65b9da7e4fb6c3cefce4fdd2769ef97fc
DmattyD <46036547+DmattyD@users.noreply.github.com>
2019-09-03 18:51:26 -0600

    Create Logical Operators - Section 11.sql
    Section 11 notes and challenge queries

---
+++ Logical Operators - Section 11.sql
@@ -0,0 +1,10 @@
+-- Locial Operators
+  -- adding logic to SQL statements
+  
+  '!=' = NOT EQUAL
+  
+  SELECT title FROM books WHERE released_year !=2017
+  -- above will return every book EXCEPT those published in 2017.
+
+-- NOT LIKE
+  -- the opposite of LIKE
