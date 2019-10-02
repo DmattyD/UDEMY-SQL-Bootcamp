6c25e9226666cc29b671098cab3a63f718aa7de6
DmattyD <46036547+DmattyD@users.noreply.github.com>
2019-10-01 21:58:38 -0600

    Update app.js
    added post functionality

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
 
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -17,12 +21,19 @@ app.get('/', function(req, res){
 		var count = results[0].count;
 		console.log('successfully connected');
 			// Respond with that count
-		res.send('We have ' + count + ' users in our db');
+		//res.send('We have ' + count + ' users in our db');
+		res.render('home', {count: count});
 	});
-
-	
 });
 
+app.post('/register', function(req, res){
+	var person = {email: req.body.email	};
+	connection.query('INSERT INTO users SET ?', person, function(err, result) {
+	console.log(err);
+	console.log(result);
+	res.redirect('/');
+});
+});
 app.get('/joke', function (req, res){
 	var joke = 'What do you call a dog that does magic tricks?  A labracadabrador.';
 	res.send(joke)
